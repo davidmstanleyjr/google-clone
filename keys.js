8b42f816cdcbafcd3499f0bf1c92d4c50f41fb11
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-28 18:22:37 -0400

    trying to deploy

---
+++ keys.js
@@ -0,0 +1,5 @@
+// https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyA3UKo4xBHT7frNLP3nI8Qvbqy3JI7klDs";
+
+// https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "b00c58b4bf4b17fe8";
